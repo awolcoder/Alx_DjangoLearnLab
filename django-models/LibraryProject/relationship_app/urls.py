f5f1ef8986a5b8c697a7b20da7da807553fc671d
awolcoder <abdulbasitawil42@gmail.com>
2025-09-02 19:12:15 +0300

    new comment

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,27 +1,27 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 from .views import add_book, edit_book, delete_book
-from . import views 
 
-urlpatterns += [
+urlpatterns = [
+    # Book views
+    path('books/', views.list_books, name='list_books'),
     path('book/add/', add_book, name='add_book'),
     path('book/edit/<int:pk>/', edit_book, name='edit_book'),
     path('book/delete/<int:pk>/', delete_book, name='delete_book'),
-]
 
-
-urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
+    # Library view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # Authentication URLs 
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
+    path('register/', views.register_view, name='register'),
 
     # Role-based views
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
 ]
+
 
