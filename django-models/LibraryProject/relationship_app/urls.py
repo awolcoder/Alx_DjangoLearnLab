325ea6112adec891edfa201f9936cea106faef28
awolcoder <abdulbasitawil42@gmail.com>
2025-09-02 19:17:38 +0300

    new comment

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,14 +13,21 @@ from .views import (
 )
 
 urlpatterns = [
+    # Book views
     path('books/', list_books, name='list_books'),
     path('book/add/', add_book, name='add_book'),
     path('book/edit/<int:pk>/', edit_book, name='edit_book'),
     path('book/delete/<int:pk>/', delete_book, name='delete_book'),
+
+    # Library view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication URLs 
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', register_view, name='register'),
+
+    # Role-based views
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
