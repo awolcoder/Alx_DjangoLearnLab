a4ef5776a37998d2391e63ea91d50fc055d0363c
awolcoder <abdulbasitawil42@gmail.com>
2025-09-02 15:39:18 +0300

    fixed query_samples.py file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,38 +1,39 @@
 import os
 import django
 
-# Set up Django environment
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.LibraryProject.settings')
 django.setup()
 
-from relationship_app.models import Author, Library
-
-# 1. Query all books by a specific author
+from relationship_app.models import Author, Book, Library
 
 author_name = "J.K. Rowling"
+library_name = "Central Library"
+
+
+# 1. Query all books by a specific author using filter()
 
 try:
     author = Author.objects.get(name=author_name)
-    books_by_author = author.books.all()
+    books_by_author = Book.objects.filter(author=author)  
     print(f"Books by {author_name}:")
     for book in books_by_author:
         print(f"- {book.title}")
 except Author.DoesNotExist:
     print(f"No author found with name '{author_name}'")
 
+
 # 2. List all books in a specific library
 
-library_name = "Central Library"
-
 try:
-    library = Library.objects.get(name=library_name)  
+    library = Library.objects.get(name=library_name)
     print(f"\nBooks in {library_name}:")
     for book in library.books.all():
         print(f"- {book.title}")
 except Library.DoesNotExist:
     print(f"No library found with name '{library_name}'")
 
-#  3. Retrieve the librarian for the library
+
+# 3. Retrieve the librarian for the library
 
 try:
     librarian = library.librarian
