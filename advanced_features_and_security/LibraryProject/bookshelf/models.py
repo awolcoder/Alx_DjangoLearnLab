d4c9d79c12aa886d16f09a27868639257f51b400
awolcoder <abdulbasitawil42@gmail.com>
2025-09-20 23:25:52 +0300

    new commits

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,5 +5,9 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
