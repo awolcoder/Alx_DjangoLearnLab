83a2fc124e498b6e739a727f5de15f6f22db147a
awolcoder <abdulbasitawil42@gmail.com>
2025-09-20 23:32:43 +0300

    new commits

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -66,4 +66,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Book
 
-# Register the Book model
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year') 
     search_fields = ('title', 'author')  
-    list_filter = ('publication_year',)  
+    list_filter = ('publication_year',)
+
+admin.site.register(CustomUser, UserAdmin)
