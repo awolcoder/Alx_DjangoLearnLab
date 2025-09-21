5dae14eb0fa0506d3e22ca999c34cc0c88a11498
awolcoder <abdulbasitawil42@gmail.com>
2025-09-21 12:54:39 +0300

    new commits

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
@@ -10,3 +11,8 @@ class BookList(ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
