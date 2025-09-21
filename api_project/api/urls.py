21da486cd8de6698c5efef3fb56b9d4ee080ce28
awolcoder <abdulbasitawil42@gmail.com>
2025-09-21 13:02:23 +0300

    new commits

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
-from rest_framework.authtoken.views import obtain_a
-uth_token
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -11,8 +11,4 @@ class BookList(ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-class BookViewSet(ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
