fc4ac88e58a0c0e866cdcfecd8e2cbe67a73a5fc
awolcoder <abdulbasitawil42@gmail.com>
2025-09-28 16:03:15 +0300

    new commits

--- advanced_api_project/advanced_api_project/urls.py
+++ advanced_api_project/advanced_api_project/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
 
+def home(request):
+    return HttpResponse("Welcome to the Advanced API Project!")
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]
