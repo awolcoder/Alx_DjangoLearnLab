ab3eaa05b7d018fee5e9208ff3ef9655853b3626
awolcoder <abdulbasitawil42@gmail.com>
2025-10-06 00:22:24 +0300

    blog project

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+# Register Post with tags
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "published_date")
+    list_filter = ("author", "published_date")
+    search_fields = ("title", "content")
+
+# Register Comment
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post", "author", "created_at", "updated_at")
+    search_fields = ("content", "author__username", "post__title")
