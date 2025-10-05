f0f45b0ca453ba79fae26b8b9d21ef462dcde016
awolcoder <abdulbasitawil42@gmail.com>
2025-10-06 00:08:23 +0300

    blog project

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
-from django.urls import reverse
+#from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
