7c1d027d45cbc594832eb574cc641400395e69af
awolcoder <abdulbasitawil42@gmail.com>
2025-10-05 23:33:15 +0300

    blog project

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
 
    # Comment CRUD
-    path("post/<int:post_id>/comment/new/", views.CommentCreateView.as_view(), name="comment_create"),
+    path("post/<int:pk>/comments/new/", views.CommentCreateView.as_view(), name="comment_create"),
     path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment_update"),
     path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
+
 ]
