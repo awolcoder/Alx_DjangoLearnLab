0eb292e9fc4e60afa66b08cd33b7fb315fddab83
awolcoder <abdulbasitawil42@gmail.com>
2025-10-12 22:19:31 +0300

    Add serializers for Post and Comment with nested CommentSerializer in PostSerializer

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -16,3 +16,4 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at', 'comments']
+
