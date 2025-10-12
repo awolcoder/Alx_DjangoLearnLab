2e38755f1daecc418f0f2b0c4005a9633b8ec585
awolcoder <abdulbasitawil42@gmail.com>
2025-10-12 17:55:43 +0300

    Create accounts app for user authentication and management

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from .models import User
+from .serializers import UserRegisterSerializer, LoginSerializer, UserProfileSerializer
 
-# Create your views here.
+# Register View
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+
+
+# Login View
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+# Profile View
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
