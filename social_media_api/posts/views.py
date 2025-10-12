aecc5306fb8cbb65b86d61a04b28b33a9f1d6cd9
awolcoder <abdulbasitawil42@gmail.com>
2025-10-12 23:10:14 +0300

    views fixed

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, generics
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from .models import Post, Comment
